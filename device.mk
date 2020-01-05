64012b465beca0625bfbbce670bd1e0fd6f64820
mukesh22584 <mks22584@gmail.com>
2020-01-05 17:20:21 +0530

    hotdogb: Welcome to Xtended

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/oneplus/hotdogb/hotdogb-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosip
+    $(LOCAL_PATH)/overlay-xtended
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/frameworks/base/packages/SystemUI
