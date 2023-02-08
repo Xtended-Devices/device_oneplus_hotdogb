a9e936fa2d5079fccfae2da5317ede60901106c3
AnierinB <anierin@evolution-x.org>
2023-02-08 20:10:22 +0530

    hotdogb: Configure OPlusExtras device specific features

--- device.mk
+++ device.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     ApertureResTarget \
+    OPlusExtrasResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
